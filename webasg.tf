9e5b9d142c61f547f49c26f7418877758406b883
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-03-26 18:10:13 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -14,7 +14,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 ###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
-  image_id      = "ami-0ec10929233384c7f"
+  image_id      = "ami-0f559c3642608c138"
   instance_type = "t3.small"
 
   network_interfaces {
